b223401f7ff07432486e3171c23b01e5125e424f
Ankur Kumar <Rai7091@gmail.com>
2024-07-28 16:38:17 +0530

    storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -7,7 +7,7 @@ export const storage = defineStorage({
             allow.guest.to(['read']),
             allow.entity('identity').to(['read', 'write', 'delete'])
         ],
-        'pink1/*': [
+        'ink/*': [
             allow.authenticated.to(['read', 'write']),
             allow.guest.to(['read', 'write'])
         ],
